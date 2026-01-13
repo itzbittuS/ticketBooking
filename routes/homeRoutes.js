534b83bc444597c353ec1d3930810e78686fe3e9
Sahani Bittu kumar <sahanibittu053@gmail.com>
2026-01-13 17:36:12 +0530

    Update homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -9,7 +9,7 @@ router.get('/registration', adminController.registrationPage);
 router.post('/registration', adminController.registerUser);
 router.get('/login', adminController.loginPage);
 router.post('/login', adminController.loginUser,);
-router.get('/admin', adminController.adminPage);
+router.get('/admin', authCookie, adminController.adminPage);
 router.get('/booking/:id', homeController.getShowsPage);
 router.get('/booking/seats/:id', homeController.seats);
 router.post('/booking', homeController.book);
